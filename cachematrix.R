855d25313bff55f777d7052d31bbfd260916c636
erobesyn <erobesyn@hotmail.com>
2014-06-18 23:55:44 +0200

    commit cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,18 +4,17 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-
-  m<-NULL
-  set<-function(y) {
-        x<<-y
-        m<<-NULL
-  }
-  get<-function() x
-  setinverse<-function(solve) m <<- inverse
-  getinverse<-function() m
-  list(set=set,get=get
-       setinverse=setinverse
-       getinverse=getinverse)
+          m<-NULL
+          set<-function(y) {
+                x<<-y
+                m<<-NULL
+          }
+          get<-function() x
+          setinverse<-function(solve) m <<- inverse
+          getinverse<-function() m
+          list(set=set,get=get
+               setinverse=setinverse
+               getinverse=getinverse)
 }
 
 
